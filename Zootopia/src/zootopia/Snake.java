e66e47aacd5cd162522699c6dcc0da8669c66a09
cendhikaimantoro <cendhika.i@gmail.com>
2016-04-26 21:44:34 +0700

    Snake
    nambah komentar

--- Zootopia/src/zootopia/Snake.java
+++ Zootopia/src/zootopia/Snake.java
@@ -5,46 +5,108 @@
  */
 package zootopia;
 /**
- *
- * @author ASUS-A451LB
+ * Kelas yang merupakan representasi player dalam game.
+ * @author Cendhika Imantoro / 13514037
  */
 public class Snake {
 
+  /**
+  * Konstanta arah atas.
+  */
   private static final char UP = 'U';
+  
+  /**
+  * Konstanta arah bawah.
+  */
   private static final char DOWN = 'D';
+  
+  /**
+  * Konstanta arah kiri.
+  */
   private static final char LEFT = 'L';
+  
+  /**
+  * Konstanta arah kanan.
+  */
   private static final char RIGHT = 'R';
+  
+  /**
+  * Konstanta arah null.
+  */
   private static final char NULL = 'N';
+  
+  /**
+  * Konstanta posisi standar.
+  */
   private static final Point DEFAULT_POSITION = new Point(0, 0);
 
+  /**
+  * Kelas bagian badan Snake.
+  */
   public class SnakeBody {
 
+    /**
+    * Atribut posisi bagian badan Snake.
+    */
     private final Point pos = new Point(DEFAULT_POSITION);
+    
+    /**
+    * Atribut arah badan snake sebelum badan snake ini.
+    */
     private char from = NULL;
+    
+    /**
+    * Atribut letak badan snake setelah badan snake ini.
+    */
     private char to = NULL;
+    
+    /**
+    * Atribut badan snake setelah badan snake ini.
+    */
     private SnakeBody next = null;
 
+    /**
+    * Getter atribut pos.
+    */
     public final Point getPos() {
       return pos;
     }
 
+    /**
+    * Getter atribut from.
+    */
     public final char getFrom() {
       return from;
     }
 
+    /**
+    * Getter atribut to.
+    */
     public final char getTo() {
       return to;
     }
 
+    /**
+    * Getter atribut next.
+    */
     public final SnakeBody getNext() {
       return next;
     }
 
+    /**
+    * Setter atribut pos.
+    * @param x
+    * @param y
+    */
     public final void setPos(final int x, final int y) {
       pos.setX(x);
       pos.setY(y);
     }
 
+    /**
+    * Setter atribut pos.
+    * @param p
+    */
     public final void setPos(final Point p) {
       if (p != null) {
         pos.setX(p.getX());
@@ -52,27 +114,70 @@ public class Snake {
       }
     }
 
+    /**
+    * Setter atribut from.
+    * @param c
+    */
     public final void setFrom(final char c) {
       from = c;
     }
 
+    /**
+    * Setter atribut to.
+    * @param c
+    */
     public final void setTo(final char c) {
       to = c;
     }
 
+    /**
+    * Setter atribut next.
+    * @param s
+    */
     public final void setNext(final SnakeBody s) {
       next = s;
     }
   }
 
+  /**
+  * Konstanta arah gerak default.
+  */
   private static final char DEFAULT_DIRECTION = RIGHT;
+  
+  /**
+  * Konstanta panjang default.
+  */
   private static final int DEFAULT_LENGTH = 3;
+  
+  /**
+  * Atribut ukuran dunia.
+  */
   private final int worldsize;
+  
+  /**
+  * Atribut kepala snake.
+  */
   private SnakeBody head;
+  
+  /**
+  * Atribut ekor snake.
+  */
   private SnakeBody tail;
+  
+  /**
+  * Atribut panjang snake.
+  */
   private int length;
+  
+  /**
+  * Atribut arah gerak snake.
+  */
   private char direction = DEFAULT_DIRECTION;
 
+  /**
+  * Konstruktor snake dengan parameter ukuran dunia
+  * @param size
+  */
   Snake(int size) {
     head = new SnakeBody();
     tail = new SnakeBody();
@@ -94,38 +199,69 @@ public class Snake {
     worldsize = size;
   }  
 
+  /**
+  * Getter atribut tail.
+  */
   public final SnakeBody getTail() {
     return tail;
   }
 
+  /**
+  * Getter atribut head.
+  */
   public final SnakeBody getHead() {
     return head;
   }
 
+  /**
+  * Getter atribut direction.
+  */
   public final char getDirection() {
     return direction;
   }
 
+  /**
+  * Getter atribut length.
+  */
   public final int getLength() {
     return length;
   }
 
+  /**
+  * Setter atribut tail.
+  * @param newTail
+  */
   public final void setTail(final SnakeBody newTail) {
     tail = newTail;
   }
 
+  /**
+  * Setter atribut head.
+  * @param newHead
+  */
   public final void setHead(final SnakeBody newHead) {
     head = newHead;
   }
 
+  /**
+  * Setter atribut direction.
+  * @param newDirection
+  */
   public final void setDirection(final char newDirection) {
     direction = newDirection;
   }
 
+  /**
+  * Setter atribut length
+  * @param newLength
+  */
   public final void setLength(final int newLength) {
     length = newLength;
   }
 
+  /**
+  * Prosedur menggerakkan snake satu langkah.
+  */
   public final void move() {
     if (getDirection() == UP) {
       getTail().setFrom(DOWN);
@@ -153,6 +289,9 @@ public class Snake {
     getHead().setTo(NULL);
   }
 
+  /**
+  * Prosedur mengubah snake ketika memakan
+  */
   public final void eat() {
     SnakeBody temp = new SnakeBody();
 
@@ -181,7 +320,5 @@ public class Snake {
    
     setLength(getLength() + 1);
   }
-
-  
 }
 
